6a4425678b83533fd59f40f7046bf1ad4c2b5cfd
Alex Som <alex.som86@gmail.com>
2022-02-17 16:02:17 +0600

    react class state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,69 @@
 import './App.css';
 import Person from './components/Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-      {/* <img src="images/avatar.png" alt="" /> */}
-      <div className="wrapper">
-        <Person name="Mike" age="20" />
-        <Person name="John" age="40" />
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    people: [
+      { name: "Mike", age: 20, prof: "Web Developer" },
+      { name: "John", age: 40, prof: "UI/UX Designer" },
+    ],
+    title: "Hello React",
+  };
+
+  changeName = () => {
+    const people = [...this.state.people];
+    const person = { ...this.state.people[0] };
+
+    person.name = "Sam";
+    people[0] = person;
+
+    this.setState({ people });
+  };
+
+  increaseAge = () => {
+    const people = this.state.people.map((person) => {
+      return {
+        ...person,
+        age: ++person.age,
+      };
+    });
+
+    this.setState({ people });
+  };
+
+  changeTitle = () => {
+    this.setState({
+      title: "Bye React"
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 onClick={this.changeTitle}>{this.state.title}</h1>
+
+        <button onClick={this.changeName}>Изменить имя</button>
+        <button onClick={this.increaseAge}>Увеличить возраст</button>
+
+        <div className="wrapper">
+          <Person
+            name={this.state.people[0].name}
+            age={this.state.people[0].age}
+          >
+            {this.state.people[0].prof}
+          </Person>
+          <Person
+            name={this.state.people[1].name}
+            age={this.state.people[1].age}
+          >
+            {this.state.people[1].prof}
+          </Person>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import './Person.css'
 import avatar from './avatar.png';
 
-function Person({name, age}) { // {name: "Mike", age: 30}
+function Person({name, age, children}) { // {name: "Mike", age: 30}
   return (
     <div className="person">
       <img className="person-img" src={avatar} alt="" />
       <h3 className="title">{name}</h3>
-      <p className="age">Age: <span>{age}</span></p>
+      <p className="age">
+        Age: <span>{age}</span>
+      </p>
+      <p className="proffession">
+        Профессия: {children}
+      </p>
     </div>
-  )
+  );
 }
 
 export default Person
