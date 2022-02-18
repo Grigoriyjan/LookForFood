8ff3baf831181b5938ae5a2b4117e3faf5019fdc
Alex Som <alex.som86@gmail.com>
2022-02-18 15:52:20 +0600

    add react list

--- src/App.js
+++ src/App.js
@@ -6,61 +6,86 @@ import React, { Component } from 'react';
 class App extends Component {
   state = {
     people: [
-      { name: "Mike", age: 20, prof: "Web Developer" },
-      { name: "John", age: 40, prof: "UI/UX Designer" },
+      { name: "Mike", age: 20, prof: "Web Developer", id: 1 },
+      { name: "John", age: 40, prof: "UI/UX Designer", id: 2 },
+      { name: "Sam", age: 30, prof: "Граф Designer", id: 3 },
+      { name: "Ketty", age: 50, prof: "Backend Developer", id: 4 },
     ],
     title: "Hello React",
+    isShow: true,
   };
 
-  changeName = () => {
+  changeName = (event, id) => {
+    const index = this.state.people.findIndex((person) => person.id === id);
     const people = [...this.state.people];
-    const person = { ...this.state.people[0] };
+    const person = { ...this.state.people[index] };
 
-    person.name = "Sam";
-    people[0] = person;
+    person.name = event.target.value;
+    people[index] = person;
 
     this.setState({ people });
   };
 
-  increaseAge = () => {
-    const people = this.state.people.map((person) => {
-      return {
-        ...person,
-        age: ++person.age,
-      };
-    });
+  increaseAge = (id) => {
+    const index = this.state.people.findIndex(person => person.id === id);
+    const people = [...this.state.people];
+    const person = { ...this.state.people[index] };
+
+    person.age++;
+    people[index] = person;
 
     this.setState({ people });
   };
 
   changeTitle = () => {
     this.setState({
-      title: "Bye React"
-    })
+      title: "Bye React",
+    });
+  };
+
+  toggle = () => {
+    this.setState({
+      isShow: !this.state.isShow, // false
+    });
+  };
+
+  removePerson = (id) => {
+    const people = this.state.people.filter(person => person.id !== id);
+
+    this.setState({people});
   }
 
   render() {
+    let wrapper = null;
+
+    if (this.state.isShow) {
+      wrapper = (
+        <div className="wrapper">
+          {
+            this.state.people.map( (person) => {
+              return (
+                <Person
+                  key={person.id}
+                  name={person.name}
+                  age={person.age}
+                  click={() => this.increaseAge(person.id)}
+                  change={(event) => this.changeName(event, person.id)}
+                  remove={() => this.removePerson(person.id)}
+                >
+                  {person.prof}
+                </Person>
+              );
+            })
+          }
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1 onClick={this.changeTitle}>{this.state.title}</h1>
-
-        <button onClick={this.changeName}>Изменить имя</button>
-        <button onClick={this.increaseAge}>Увеличить возраст</button>
-
-        <div className="wrapper">
-          <Person
-            name={this.state.people[0].name}
-            age={this.state.people[0].age}
-          >
-            {this.state.people[0].prof}
-          </Person>
-          <Person
-            name={this.state.people[1].name}
-            age={this.state.people[1].age}
-          >
-            {this.state.people[1].prof}
-          </Person>
-        </div>
+        <button onClick={this.toggle}>Переключить Юзеров</button>
+        {wrapper}
       </div>
     );
   }
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import './Person.css'
 import avatar from './avatar.png';
 
-function Person({name, age, children}) { // {name: "Mike", age: 30}
+function Person({ name, age, children, click, change, remove }) {
+  // {name: "Mike", age: 30}
   return (
     <div className="person">
-      <img className="person-img" src={avatar} alt="" />
-      <h3 className="title">{name}</h3>
+      <img onClick={click} className="person-img" src={avatar} alt="" />
+      <h3 className="title" onClick={remove}>{name}</h3>
+      <input
+        type="text"
+        className="change__name"
+        value={name}
+        onChange={change}
+      />
       <p className="age">
         Age: <span>{age}</span>
       </p>
-      <p className="proffession">
-        Профессия: {children}
-      </p>
+      <p className="proffession">Профессия: {children}</p>
     </div>
   );
 }
